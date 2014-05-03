27cae18100bc9759a9e85c7c289e7d4da4b28e6f
Dan Haskin <dhaskin@adaptivecomputing.com>
2014-05-03 13:09:57 -0600

    Print out 2.1, start on 2.2

--- chapter2.ml
+++ chapter2.ml
@@ -1,2 +1,27 @@
+let rec print_int_list l =
+    match l with
+    | [] -> ()
+    | e::t -> print_int e ; print_string " " ; print_int_list t;;
+
+let rec print_int_list_list =
+    function
+        [] -> ()
+    | e::t -> print_string "[ ";
+              print_int_list e;
+              print_string "] ";
+              (print_int_list_list t);;
+
 (* exercise 2.1 *)
-let rec suffixes t = match t with | [] -> [[]] | (h::e) -> t :: suffixes e in suffixes [1,2,3,4];;
+print_string "2.1)\n"; print_string "    ";
+let rec suffixes =
+   function
+      [] -> [[]]
+      | h::t -> ((h::t) :: suffixes t) in
+print_int_list_list (suffixes [1;2;3;4]);
+print_string "\n";;
+
+(* definitions needed from figure 2.9 *)
+module type Ordered = sig
+    val lt : 'a -> 'a -> bool
+    val eq : 'a -> 'a -> bool
+end;;
