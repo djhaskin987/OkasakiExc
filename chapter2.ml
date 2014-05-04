3d0c9c7b090e3f2a055bb1f2dd8fccca11c79d81
Dan Haskin <dhaskin@adaptivecomputing.com>
2014-05-03 22:35:20 -0600

    clip

--- chapter2.ml
+++ chapter2.ml
@@ -21,7 +21,50 @@ print_int_list_list (suffixes [1;2;3;4]);
 print_string "\n";;
 
 (* definitions needed from figure 2.9 *)
-module type Ordered = sig
-    val lt : 'a -> 'a -> bool
-    val eq : 'a -> 'a -> bool
+module type ORDERED = sig
+    type t
+    val lt : t -> t -> bool
+    val print : t -> unit
 end;;
+
+module UnbalancedSet =
+    functor (Elt : ORDERED) ->
+        struct
+            type elem = Elt.t
+            type tree = Empty | T of tree * elem * tree
+            type set = tree
+            let empty = Empty
+            let rec member =
+                fun x t ->
+                    match t with
+                    | Empty -> false
+                    | T(a,y,b) ->
+                            if (Elt.lt x y) then (member x a)
+                            else if (Elt.lt y x) then (member x b)
+                            else true
+            let rec ins =
+                fun x t ->
+                    match t with
+                    | Empty -> T(Empty, x, Empty)
+                    | T(a,y,b) ->
+                            if (Elt.lt x y) then T((ins x a),y,b)
+                            else if (Elt.lt y x) then T(a,y,(ins x b))
+                            else T(a,x,b)
+        end;;
+
+module OrderedInt : ORDERED =
+    struct
+        type t = int
+        let lt = fun (x : int) (y : int) ->
+            x < y
+        let print = fun (x : int) ->
+            print_int x
+    end;;
+module USetInt = UnbalancedSet(OrderedInt);;
+USetInt.ins (OrderedInt.id 5) USetInt.empty;;
+
+(*
+            (USetInt.ins (OrderedInt.t 4)
+              (USetInt.ins 6
+                (USetInt.ins 8 USetInt.Empty) ) ) );;
+print_bool member 5; print_string " "; print_bool member 10; print_string "\n";; *)
